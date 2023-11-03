2f6a780336135aae38f3ede74fc2ad1b29b4c2f9
jdsarge <jdsargent91@gmail.com>
2023-11-03 08:25:33 -0500

    Update CardForm.js

--- src/Layout/Card/CardForm.js
+++ src/Layout/Card/CardForm.js
@@ -1,37 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 function CardForm({ changeFront, changeBack, handleSave, handleDoneCancel, cardValueFront, cardValueBack }) {
-
-    
     return (
       <form>
         <div className="mb-3">
           <label className="form-label">Front</label>
-            <textarea
-              type="text" className="form-control" 
-              id="front"
-              required
-              placeholder="Front side of the card"
-              value={cardValueFront}
-              rows="3"
-              onChange={changeFront}/> 
-          </div>
+          <textarea
+            type="text" 
+            className="form-control" 
+            id="front"
+            name="front"
+            required
+            placeholder="Front side of the card"
+            value={cardValueFront}
+            rows="3"
+            onChange={changeFront}
+          /> 
+        </div>
         <div className="mb-3">
           <label className="form-label">Back</label>
-            <textarea 
+          <textarea 
             className="form-control" 
             id="back" 
+            name="back"
             required
             placeholder="Back side of card"
             value={cardValueBack}
             rows="3"
             onChange={changeBack}
-            />
+          />
         </div>
-          <button type="done" className="btn btn-secondary" onClick={handleDoneCancel}>Done</button>
-          <button type="submit" className="btn btn-primary mx-1" onClick={handleSave}>Save</button>
+        <button type="button" className="btn btn-secondary" onClick={handleDoneCancel}>Done</button>
+        <button type="submit" className="btn btn-primary mx-1" onClick={handleSave}>Save</button>
       </form>
     );
-  }
+}
 
-  export default CardForm
+export default CardForm;
